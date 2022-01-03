8abdd8064918de371dc7204f3d7a8a8763ddbbcd
Emily Millard <emily_millard@live.co.uk>
2022-01-03 14:56:09 +0000

    implemented index, show, new and create for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,11 +1,32 @@
 class ProjectsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_project, only: :show
+
   def index
     @projects = Project.all
-    @project = Project.new
+  end
+
+  def show
   end
 
   def new
+    @project = current_user.projects.build
+  end
 
+  def create
+    @project = current_user.projects.build(project_params)
+    if @project.save
+      redirect_to project_path(@project)
+    end
+  end
+
+  private
+
+  def set_project
+    @project = current_user.projects.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :description)
   end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,3 +1,14 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_project
+
+  def index
+    @tasks = Task.all
+  end
+
+  private
+
+  def set_project
+    @project = current_user.projects.find(params[:project_id])
+  end
 end
